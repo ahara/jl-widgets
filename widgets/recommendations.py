263a01e2665cbb05237100cb93dd463a2f7ad742
Adam Harasimowicz <adam.harasimowicz@webinterpret.com>
2024-08-18 18:11:16 +0200

    Add basic code showing laptops

--- widgets/recommendations.py
+++ widgets/recommendations.py
@@ -1,3 +1,29 @@
+import pandas as pd
 import streamlit as st
 
 st.header("test")
+
+
+@st.cache_data
+def load_data():
+    data_path = './data/laptop_recommendation.csv'
+    _df = pd.read_csv(data_path)
+    return _df
+
+
+def print_laptop(laptop) -> st.container:
+    c = st.container()
+    col1, col2 = c.columns(2)
+    col1.image(laptop["url_image"])
+    col2.link_button(f'{laptop["name"]}', laptop["url_referal"], type='primary')
+    c.markdown("---")
+
+    return c
+
+
+df = load_data()
+
+for _, row in df.iterrows():
+    print_laptop(row)
+
+print(st.query_params)
